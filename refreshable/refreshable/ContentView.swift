5d7e55c6f872c223717c77f41930732acad09175
chanwoo <humains29@gmail.com>
2022-12-08 10:11:10 +0900

    :sparkles: Add Refreshable

---
+++ refreshable/refreshable/ContentView.swift
@@ -0,0 +1,68 @@
+//
+//  ContentView.swift
+//  refreshable
+//
+//  Created by GREEN on 2022/12/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  @State var colors: [Color] = []
+  
+  var body: some View {
+    VStack(spacing: 10) {
+      Text("Colors")
+        .font(.largeTitle)
+        .padding(.top, 50)
+      
+      List(colors, id: \.self) { color in
+        RoundedRectangle(cornerRadius: 10)
+          .frame(width: 300, height: 50)
+          .foregroundColor(color)
+      }
+      .onAppear {
+        colors = initColors()
+      }
+      .refreshable {
+        await colors.append(contentsOf: addColors())
+      }
+      
+      CustomRefresher()
+        .refreshable {
+          await colors.append(contentsOf: addColors())
+        }
+      
+      Spacer()
+    }
+  }
+  
+  private func initColors() -> [Color] {
+    return [.yellow, .green, .indigo, .orange, .purple]
+  }
+  
+  private func addColors() async -> [Color] {
+    return [.red, .blue, .gray]
+  }
+}
+
+struct CustomRefresher: View {
+  @Environment(\.refresh) private var refresh
+  
+  var body: some View {
+    Group {
+      if let refresh = refresh {
+        Button(
+          action: {
+            Task {
+              await refresh()
+            }
+          },
+          label: {
+            Text("Refresh Button")
+          }
+        )
+      }
+    }
+  }
+}
---
+++ refreshable/refreshable/refreshableApp.swift
@@ -0,0 +1,17 @@
+//
+//  refreshableApp.swift
+//  refreshable
+//
+//  Created by GREEN on 2022/12/08.
+//
+
+import SwiftUI
+
+@main
+struct refreshableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
