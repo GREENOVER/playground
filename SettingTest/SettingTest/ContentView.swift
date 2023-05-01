222a16f4fc750184ef64d8e6e019b10c54073f82
chanwoo <humains29@gmail.com>
2023-05-01 20:17:43 +0900

    :sparkles: Add Setting Bundle Test

---
+++ SettingTest/SettingTest/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SettingTest
+//
+//  Created by GREEN on 2023/05/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SettingTest/SettingTest/SettingTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SettingTestApp.swift
+//  SettingTest
+//
+//  Created by GREEN on 2023/05/01.
+//
+
+import SwiftUI
+
+@main
+struct SettingTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
