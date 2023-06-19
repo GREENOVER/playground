1019c97e3d2ff9b42eeea4cfcd5c6ec1e0bba765
chanwoo <humains29@gmail.com>
2023-06-19 15:01:17 +0900

    :sparkles: Add LicenseProject

---
+++ LicenseProject/LicenseProject/LicenseProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LicenseProjectApp.swift
+//  LicenseProject
+//
+//  Created by GREEN on 2023/06/19.
+//
+
+import SwiftUI
+
+@main
+struct LicenseProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
